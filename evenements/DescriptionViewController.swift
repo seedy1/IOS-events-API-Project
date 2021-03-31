722de5fd7b2ca40233672aa81e26b0f5e2aae686
seedy1 <jobe.seedy@gmail.com>
2021-03-31 10:46:37 +0100

    added single event screen

---
+++ evenements/DescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DescriptionViewController.swift
+//  evenements
+//
+//  Created by user188225 on 3/31/21.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Single Event View"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
