267d9973c9860573c0a78da287182d4efced9006
Andriatiana Victor <rakotondranorovictor@gmail.com>
2021-04-07 09:43:28 +0200

    Added all services

--- evenements/Entity/Speaker.swift
+++ evenements/Entity/Speaker.swift
@@ -7,7 +7,97 @@
 
 import Foundation
 
+// App model
 struct Speaker {
-    var name: String;
-    
+    var id: String
+    var role: String
+    var company: [String]
+    var status: String?
+    var email: String
+    var type: String
+    var speakingAt: [String]?
+    var name: String
+    var phone: String
+    var createdTime: String
 }
+
+// Schedule mapping to service
+extension Speaker {
+    init(from raw: SpeakerService.RawSpeaker.Record) {
+        id = raw.id;
+        role = raw.fields.role
+        company = raw.fields.company
+        status =  raw.fields.status
+        email =  raw.fields.email
+        type = raw.fields.type
+        speakingAt = raw.fields.speakingAt
+        name = raw.fields.name
+        phone = raw.fields.phone
+        createdTime = raw.createdTime;
+    }
+}
+
+// EventLocation service
+class SpeakerService {
+    
+    // Fetch all
+    func fetchURL(urlString: String,
+                  onNewSpeakerHandler: @escaping ([Speaker]) -> Void){
+        //call fetcher service
+        FetcherService().fetchJSON(fetchUrlString: urlString){ (speakers:RawSpeaker) in
+            let speakerList = speakers.records.map { Speaker(from: $0) }
+            // return to main thread
+            DispatchQueue.main.async {
+                /* Do async work here */
+                onNewSpeakerHandler(speakerList)
+            }
+        }
+    }
+    
+    
+    
+    // Inner service model serving as an intermediate for managing API
+    struct RawSpeaker: Decodable {
+        
+        // List of records
+        var records: [Record]
+        enum RawKeys: String, CodingKey{
+            case records = "records"
+        }
+        
+        struct Record: Codable {
+            var id: String
+            var fields: Fields
+            var createdTime: String
+
+            enum CodingKeys: String, CodingKey {
+                case id = "id"
+                case fields = "fields"
+                case createdTime = "createdTime"
+            }
+        }
+
+        struct Fields: Codable {
+            var role: String
+            var company: [String]
+            var status: String?
+            var email: String
+            var type: String
+            var speakingAt: [String]?
+            var name: String
+            var phone: String
+
+            enum CodingKeys: String, CodingKey {
+                case role = "Role"
+                case company = "Company"
+                case status = "Status"
+                case email = "Email"
+                case type = "Type"
+                case speakingAt = "Speaking at"
+                case name = "Name"
+                case phone = "Phone"
+            }
+        }
+    }
+}
+
---
+++ evenements/Entity/Sponsor.swift
@@ -0,0 +1,94 @@
+//
+//  Sponsor.swift
+//  evenements
+//
+//  Created by RAKOTONDRANORO Andriatiana Victor on 07/04/2021.
+//
+
+import Foundation
+
+// App model
+struct Sponsor {
+    var id: String
+    var contacts: [String]?
+    var company: String
+    var status: String
+    var sponsoredAmount: Int?
+    var notes: String?
+    var previousSponsor: Bool?
+    var createdTime: String
+}
+
+// Schedule mapping to service
+extension Sponsor {
+    init(from raw: SponsorService.RawSponsor.Record) {
+        id = raw.id;
+        contacts = raw.fields.contactS
+        company = raw.fields.company
+        status =  raw.fields.status
+        sponsoredAmount = raw.fields.sponsoredAmount
+        notes = raw.fields.notes
+        previousSponsor = raw.fields.previousSponsor
+        createdTime = raw.createdTime;
+    }
+}
+
+// EventLocation service
+class SponsorService {
+    
+    // Fetch all
+    func fetchURL(urlString: String,
+                  onNewSponsorHandler: @escaping ([Sponsor]) -> Void){
+        //call fetcher service
+        FetcherService().fetchJSON(fetchUrlString: urlString){ (sponsors:RawSponsor) in
+            let sponsorsList = sponsors.records.map { Sponsor(from: $0) }
+            // return to main thread
+            DispatchQueue.main.async {
+                /* Do async work here */
+                onNewSponsorHandler(sponsorsList)
+            }
+        }
+    }
+    
+    
+    
+    // Inner service model serving as an intermediate for managing API
+    struct RawSponsor: Decodable {
+        
+        // List of records
+        var records: [Record]
+        enum RawKeys: String, CodingKey{
+            case records = "records"
+        }
+        
+        struct Record: Codable {
+            var id: String
+            var fields: Fields
+            var createdTime: String
+
+            enum CodingKeys: String, CodingKey {
+                case id = "id"
+                case fields = "fields"
+                case createdTime = "createdTime"
+            }
+        }
+
+        struct Fields: Codable {
+            var contactS: [String]?
+            var company: String
+            var status: String
+            var sponsoredAmount: Int?
+            var notes: String?
+            var previousSponsor: Bool?
+
+            enum CodingKeys: String, CodingKey {
+                case contactS = "Contact(s)"
+                case company = "Company"
+                case status = "Status"
+                case sponsoredAmount = "Sponsored amount"
+                case notes = "Notes"
+                case previousSponsor = "Previous sponsor"
+            }
+        }
+    }
+}
